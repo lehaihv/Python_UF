4fea57ce60ba1c02c16b5a0be8869b88f54ddf3f
Hai LE <69892340+lehaihv@users.noreply.github.com>
2023-12-09 20:59:43 +0700

    Mac 5530

--- Data_Analy/main.py
+++ Data_Analy/main.py
@@ -4,11 +4,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 citi_bikes = pd.read_csv("data/citibike.csv")
+
 # citi_bikes = pd.DataFrame(pd.read_csv("data/citibike.csv"))
+covid_level = pd.read_csv("data/covidlevel.csv", encoding='latin-1')
 
+# X_train, X_test, y_train, y_test = train_test_split(citi_bikes.tripduration, citi_bikes.bikeid,
+#                                                    random_state=1)
 
-X_train, X_test, y_train, y_test = train_test_split(citi_bikes.tripduration, citi_bikes.bikeid,
-                                                    random_state=1)
 
 # print(citi_bikes.shape)  # show shape of dataset including number of data points and features
 print("Shape of citibike data: {}".format(citi_bikes.shape))  # show shape of dataset including number of data points
@@ -16,8 +18,8 @@ print("Shape of citibike data: {}".format(citi_bikes.shape))  # show shape of da
 # print("citibike.keys(): \n{}".format(citi_bikes.keys()))  # print all feature names of dataset
 print("Feature names: \n{}".format(citi_bikes.keys()))  # print all feature names of dataset
 
-print(X_train.shape)
-print(X_test.shape)
+# print(X_train.shape)
+# print(X_test.shape)
 
 print(citi_bikes[0:5])  # Print 5 first row of dataset
 print(citi_bikes.tripduration[0:5])  # Print 5 first row of feature "tripduration" of dataset
@@ -30,5 +32,20 @@ print(citi_bikes.tail())  # Print last 5 row (tail) of dataset
 print(citi_bikes.isnull().any())  # check whether there were any rows with nulls
 print(citi_bikes[citi_bikes["birth year"].isnull()].head())  # Print first 5 row (head) with nulls of dataset
 print(citi_bikes[citi_bikes["birth year"].isnull()].tail())  # Print last 5 row (tail) with nulls of dataset
+
+
+print("Shape of citibike data: {}".format(covid_level.shape))  # show shape of dataset including number of data points
+# and features
+print("Feature names: \n{}".format(covid_level.keys()))  # print all feature names of dataset
+
+print(covid_level.head())  # Print first 5 row (head) of dataset
+print(covid_level.tail())  # Print last 5 row (tail) of dataset
+
+print(covid_level.isnull().any())  # check whether there were any rows with nulls
+print(covid_level[covid_level["health_service_area_population"].isnull()].head())  # Print first 5 row (head) with
+# nulls of dataset
+# print(covid_level[covid_level["county_population"].isnull()].tail())  # Print last 5 row (tail) with nulls of dataset
+
+
 
 
