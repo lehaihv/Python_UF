021f6f8a25598ff22d54b040ccdabc67fe7f8649
Hai LE <69892340+lehaihv@users.noreply.github.com>
2023-12-09 16:07:40 +0700

    Mac 7740

--- Data_Analy/main.py
+++ Data_Analy/main.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 citi_bikes = pd.read_csv("data/citibike.csv")
+# citi_bikes = pd.DataFrame(pd.read_csv("data/citibike.csv"))
+
 
 X_train, X_test, y_train, y_test = train_test_split(citi_bikes.tripduration, citi_bikes.bikeid,
                                                     random_state=1)
@@ -14,27 +16,19 @@ print("Shape of citibike data: {}".format(citi_bikes.shape))  # show shape of da
 # print("citibike.keys(): \n{}".format(citi_bikes.keys()))  # print all feature names of dataset
 print("Feature names: \n{}".format(citi_bikes.keys()))  # print all feature names of dataset
 
-
 print(X_train.shape)
 print(X_test.shape)
+
 print(citi_bikes[0:5])  # Print 5 first row of dataset
 print(citi_bikes.tripduration[0:5])  # Print 5 first row of feature "tripduration" of dataset
 print(citi_bikes[["tripduration", "bikeid"]][0:5])  # Print 5 first row feature "tripduration" and "bikeid" of dataset
 
+print(citi_bikes.starttime[0:5])
+print(citi_bikes.head())  # Print first 5 row (head) of dataset
+print(citi_bikes.tail())  # Print last 5 row (tail) of dataset
 
-# scaler = MinMaxScaler()
-# scaler.fit(X_train)
-
-
-
-
-
-
+print(citi_bikes.isnull().any())  # check whether there were any rows with nulls
+print(citi_bikes[citi_bikes["birth year"].isnull()].head())  # Print first 5 row (head) with nulls of dataset
+print(citi_bikes[citi_bikes["birth year"].isnull()].tail())  # Print last 5 row (tail) with nulls of dataset
 
 
-'''
-plt.semilogy(citi_bikes.starttime, citi_bikes.bikeid)
-plt.xlabel("Year")
-plt.ylabel("Price in $/Mbyte")
-plt.show()
-'''
