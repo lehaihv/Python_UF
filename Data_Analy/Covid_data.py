4ffc03dfb33a7edd5ea2f0ba8a3d0bbbe7fadaf8
Hai LE <69892340+lehaihv@users.noreply.github.com>
2023-12-16 09:18:45 +0700

    Mac 7740

--- Data_Analy/Covid_data.py
+++ Data_Analy/Covid_data.py
@@ -6,8 +6,21 @@ from sklearn.preprocessing import MinMaxScaler
 from natsort import index_natsorted
 import seaborn as sns
 
-covid_level = pd.read_csv("data/covidlevel.csv", encoding='latin-1')
+'''
+covid_data = pd.read_csv("data/cdcdataset.csv")
 
-print("Shape of covid level data: {}".format(covid_level.shape))  # show shape of dataset including number of data
+print("Shape of covid level data: {}".format(covid_data.shape))  # show shape of dataset including number of data
 # points and features
-print("Feature names: \n{}".format(covid_level.keys()))  # print all feature names of dataset
+print("Feature names: \n{}".format(covid_data.keys()))  # print all feature names of dataset
+
+covid_data_short = pd.DataFrame(columns=['sample_collect_date', 'wwtp_name', 'pcr_target_flowpop_lin', 'pcr_target',
+                                         'pcr_gene_target_agg'])
+covid_data_short[['sample_collect_date', 'wwtp_name', 'pcr_target_flowpop_lin', 'pcr_target', 'pcr_gene_target_agg']] \
+    = covid_data[['sample_collect_date', 'wwtp_name', 'pcr_target_flowpop_lin', 'pcr_target', 'pcr_gene_target_agg']]
+
+print(covid_data_short[0:3])
+
+covid_data_short.to_csv('data/coviddata.csv')
+'''
+covid_data = pd.read_csv("data/coviddata.csv")
+print(covid_data)
