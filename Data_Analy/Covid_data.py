016f1ed1425cd2fd355ccb999842b30cdf7176e6
Hai LE <69892340+lehaihv@users.noreply.github.com>
2023-12-16 09:52:10 +0700

    Mac 7740

--- Data_Analy/Covid_data.py
+++ Data_Analy/Covid_data.py
@@ -21,6 +21,26 @@ covid_data_short[['sample_collect_date', 'wwtp_name', 'pcr_target_flowpop_lin', 
 print(covid_data_short[0:3])
 
 covid_data_short.to_csv('data/coviddata.csv')
-'''
+
 covid_data = pd.read_csv("data/coviddata.csv")
 print(covid_data)
+covid_data_remove = covid_data.dropna()  # remove/drop the rows where at least one element is missing.
+print(covid_data_remove.isnull().any())  # check whether there were any rows with nulls
+print(covid_data_remove)
+# convert to date
+covid_data_remove['sample_collect_date'] = pd.to_datetime(covid_data_remove['sample_collect_date'])
+covid_data_date_short = covid_data_remove.sort_values(by='sample_collect_date')
+print(covid_data_date_short)
+
+# covid_data_sum = covid_data_date_short.groupby('sample_collect_date').sum()
+# print(covid_data_sum)
+covid_data_date_short.to_csv('data/coviddatafinal.csv')
+'''
+
+covid_data = pd.read_csv("data/coviddatafinal.csv")
+print(covid_data)
+
+
+
+
+
