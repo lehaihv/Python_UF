72a0ff1c54716bf794d0373a2ace3a7175194619
Hai LE <69892340+lehaihv@users.noreply.github.com>
2023-12-17 22:08:43 +0700

    Update Covid_data.py

--- Data_Analy/Covid_data.py
+++ Data_Analy/Covid_data.py
@@ -38,10 +38,37 @@ covid_data_date_short.to_csv('data/coviddatafinal.csv')
 '''
 
 pd.set_option('display.max_columns', None)  # force pandas to display any/all number of columns.
+'''
 covid_data = pd.read_csv("data/coviddatafinal.csv")
-print(covid_data[0:20])
+print(covid_data)
 
+print(covid_data.groupby('wwtp_name').sum())
 
+covid_data_wtp_name = covid_data.groupby('wwtp_name').sum()
+covid_data_wtp_name.to_csv('data/coviddatafinal1.csv')
+'''
+
+covid_data1 = pd.read_csv("data/coviddatafinal1.csv")[0:50]
+print(covid_data1)
+
+# Multiple lines using pyplot
+# red dashes, blue squares and green triangles
+plt.plot(covid_data1.wwtp_name, covid_data1.pcr_target_flowpop_lin, 'r--')
+
+plt.xlabel("Date")
+plt.ylabel("Number of cases")
+plt.legend(['covid_inpatient_bed_utilization', 'covid_hospital_admissions_per_100k', 'covid_cases_per_100k'],
+           ncol=1, loc='upper right')
+
+plt.title('Covid Level in the US', fontsize=20, fontname='Times New Roman')
+
+'''# integrate LaTeX expressions (insert mathematical expressions within the chart)
+plt.text(covid_level_date_short.date_updated[42], 750000, r'$y = x^2$', fontsize=20, bbox={'facecolor': 'yellow',
+                                                                                           'alpha': 0.2})
+'''
+
+sns.set()
+plt.show()
 
 
 
